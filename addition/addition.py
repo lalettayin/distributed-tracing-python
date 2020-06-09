7d66696da54b8ea0376e4f9822dea157702cc01a
Ogun Isik <ogunisik@gmail.com>
2020-06-09 15:20:55 +0200

    Add Jaeger implementation

--- addition/addition.py
+++ addition/addition.py
@@ -1,16 +1,42 @@
 import tornado.ioloop
 import tornado.web
+from jaeger_client import Config
+from opentracing import Format
+
+def initialize_tracer(service_name):
+    config = Config(config={
+            "sampler": {
+                "type": "const",
+                "param": 1,
+            },
+            "local_agent": {
+                "reporting_host": "jaeger",
+                "reporting_port": "6831",
+            },
+            "logging": True,
+        }, service_name=service_name, validate=True)
+    return config.initialize_tracer(io_loop=tornado.ioloop.IOLoop.current())
 
 class MainHandler(tornado.web.RequestHandler):
+    def initialize(self, tracer):
+        self.tracer = tracer
+
     async def get(self):
-        x = int(self.get_argument("x", 0))
-        y = int(self.get_argument("y", 0))
-        result = x + y
-        self.write(str(result))
+        span_ctx = self.tracer.extract(Format.HTTP_HEADERS, self.request.headers)
+        with self.tracer.start_span("add", child_of=span_ctx) as span:
+            x = int(self.get_argument("x", 0))
+            y = int(self.get_argument("y", 0))
+
+            result = x + y
+
+            span.log_kv({"x": x, "y": y, "result": result})
+
+            self.write(str(result))
 
 def make_app():
+    tracer = initialize_tracer("addition")
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/", MainHandler, dict(tracer=tracer)),
     ])
 
 def main():
--- multiplication/multiplication.py
+++ multiplication/multiplication.py
@@ -3,25 +3,52 @@ import tornado.ioloop
 import tornado.web
 from tornado.httpclient import AsyncHTTPClient
 from tornado.httputil import url_concat
+from jaeger_client import Config
+from opentracing import Format
+
+def initialize_tracer(service_name):
+    config = Config(config={
+            "sampler": {
+                "type": "const",
+                "param": 1,
+            },
+            "local_agent": {
+                "reporting_host": "jaeger",
+                "reporting_port": "6831",
+            },
+            "logging": True,
+        }, service_name=service_name, validate=True)
+    return config.initialize_tracer(io_loop=tornado.ioloop.IOLoop.current())
 
 class MainHandler(tornado.web.RequestHandler):
+    def initialize(self, tracer):
+        self.tracer = tracer
+
     async def get(self):
-        x = int(self.get_argument("x", 0))
-        y = int(self.get_argument("y", 0))
+        with self.tracer.start_span("multiply") as span:
+            x = int(self.get_argument("x", 0))
+            y = int(self.get_argument("y", 0))
 
-        total =  0 if (x % 2) == 0 else y
-        http = AsyncHTTPClient()
-        for _ in range(x // 2):
-            payload = {"x": y, "y": y}
-            url = url_concat(os.getenv("ADDITION_URL"), payload)
-            response = await http.fetch(url)
-            total += int(response.body)
+            total =  0 if (x % 2) == 0 else y
+            http = AsyncHTTPClient()
 
-        self.write(str(total))
+            headers = {}
+            self.tracer.inject(span, Format.HTTP_HEADERS, headers)
+            
+            for _ in range(x // 2):
+                payload = {"x": y, "y": y}
+                url = url_concat(os.getenv("ADDITION_URL"), payload)
+                response = await http.fetch(url, headers=headers)
+                total += int(response.body)
+
+            span.log_kv({"x": x, "y": y, "result": total})
+
+            self.write(str(total))
 
 def make_app():
+    tracer = initialize_tracer("multiplication")
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/", MainHandler, dict(tracer=tracer)),
     ])
 
 def main():
